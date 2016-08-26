31bed48c9a2612b947c43c33919a672dfdb78983
Michael <michaelthomas372@gmail.com>
2016-08-26 15:52:12 +0200

    updated to use laravel 5.3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
-		$this->call(PollAndOptionSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
